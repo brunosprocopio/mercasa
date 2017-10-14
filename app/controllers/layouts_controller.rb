e7f4d2dd47032ff8f6b51ff698d49fdaafcd8c77
lucascorreaaa <lucascorreasilvaa@gmail.com>
2017-10-14 18:10:21 -0300

    Linkando home page ao market

---
+++ app/controllers/layouts_controller.rb
@@ -0,0 +1,4 @@
+class LayoutsController < ApplicationController
+    def market
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'layouts/market'
+
   resources :users
   resources :products
 
+  #get "/layouts/market" => "layouts#market"
   root 'home#index'
 end
